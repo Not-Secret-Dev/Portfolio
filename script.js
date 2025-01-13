06330b720c8dcf81222dbb948fb0073ce9086768
Not-Secret-Dev <ayaanmumtaz8@gmail.com>
2025-01-13 23:08:46 +0500

    About section and navlinks modifications

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const dropDownMenuBtn = document.querySelector("#dropdown-sc-nav button");
 const dropDownMenu = document.querySelector("#dropdown-sc-nav ul");
+const NavBtns = document.querySelectorAll("nav ul li");
+const NavSecs = document.querySelectorAll("section");
 
 dropDownMenuBtn.addEventListener("click", () => {
   dropDownMenu.classList.toggle("show");
+});
+
+NavBtns.forEach((NavBtn) => {
+  NavBtn.addEventListener("click", () => {
+    NavSecs.forEach((NavSec) => {
+      if (NavBtn.id === NavSec.id) {
+        if (NavSec.style.display === "none") {
+          NavSec.style.display = "block";
+        }
+      } else {
+        NavSec.style.display = "none";
+      }
+    });
+  });
 });
